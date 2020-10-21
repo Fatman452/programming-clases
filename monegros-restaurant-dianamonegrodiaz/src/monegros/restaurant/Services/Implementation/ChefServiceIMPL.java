0d4b61bfa08f1c96cf72e925fca7bed2b955e2bd
Diana Monegro <diana2monegro1@gmail.com>
2020-10-20 20:45:47 -0400

    Adding initial INSERT and DELETE impl and interfaces of Chef

---
+++ monegros-restaurant-dianamonegrodiaz/src/monegros/restaurant/Services/Implementation/ChefServiceIMPL.java
@@ -0,0 +1,21 @@
+package monegros.restaurant.services.implementation;
+
+import java.util.Optional;
+import monegros.restaurant.entities.repository.IChefRepository;
+import monegros.restaurant.services.interfaces.IChefService;
+import monegros.restaurant.entities.Chef;
+
+public class ChefServiceIMPL implements IChefService{
+	private final IChefRepository chefRepository;
+	public ChefServiceIMPL(IChefRepository chefRepository) {
+		this.chefRepository = chefRepository;
+	}
+	@Override
+	public Optional<Chef> findById(Long id) {
+		return this.chefRepository.findById(id);
+	}
+	@Override
+	public Optional<Chef> insert(Chef chef) {
+		return this.chefRepository.insert(chef);
+	}
+}
